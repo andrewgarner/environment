4cfabe1569bbaedc1f5be33d9bcefef92cec7fec
Andrew Garner <andrew@andrewgarner.com>
2024-07-31 14:49:14 +0100

    Configure AWS CLI

--- home.nix
+++ home.nix
@@ -8,6 +8,27 @@
   programs = {
     home-manager.enable = true;
 
+    awscli = {
+      enable = true;
+      settings = {
+        "default" = {
+          region = "eu-west-2";
+        };
+
+        "profile AndrewGarner-AdministratorAccess" = {
+          sso_account_id = "767397922954";
+          sso_role_name = "AdministratorAccess";
+          sso_session = "AndrewGarner";
+        };
+
+        "sso-session AndrewGarner" = {
+          sso_region = "eu-west-2";
+          sso_registration_scopes = "sso:account:access";
+          sso_start_url = "https://andrewgarner.awsapps.com/start";
+        };
+      };
+    };
+
     direnv = {
       enable = true;
       nix-direnv.enable = true;
