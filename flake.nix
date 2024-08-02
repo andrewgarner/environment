f6c43b9b1eacb11a65720b4c4a6456b3fdcecc17
Andrew Garner <andrew@andrewgarner.com>
2024-08-02 14:14:31 +0100

    Version inputs from FlakeHub

--- flake.nix
+++ flake.nix
@@ -1,6 +1,15 @@
 {
   description = "My home configuration";
 
+  inputs = {
+    home-manager = {
+      url = "https://flakehub.com/f/nix-community/home-manager/0.2405.3571.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2405.633614.tar.gz";
+  };
+
   outputs = {
     nixpkgs,
     home-manager,
