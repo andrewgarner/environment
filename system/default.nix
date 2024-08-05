f8c996cadc690d8ff98e304adeda5794d43fc126
Andrew Garner <andrew@andrewgarner.com>
2024-08-05 13:36:56 +0100

    Configure system preferences

--- system/default.nix
+++ system/default.nix
@@ -22,7 +22,37 @@
       /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
     '';
 
-    defaults.smb.NetBIOSName = profile.hostname;
+    defaults = {
+      dock.autohide = true;
+      dock.mineffect = "scale";
+      dock.minimize-to-application = true;
+      dock.persistent-apps = [
+        "/Applications/Arc.app"
+        "/System/Applications/Messages.app"
+        "/Applications/Obsidian.app"
+        "/Applications/Warp.app/"
+        "/Applications/WebStorm.app"
+        "/System/Applications/System Settings.app"
+      ];
+
+      dock.persistent-others = [
+        "/Users/${profile.username}/Downloads"
+      ];
+
+      dock.show-process-indicators = false;
+      dock.show-recents = true;
+
+      NSGlobalDomain.AppleInterfaceStyle = "Dark";
+      NSGlobalDomain.AppleShowScrollBars = "WhenScrolling";
+      NSGlobalDomain."com.apple.trackpad.scaling" = 3.0;
+
+      smb.NetBIOSName = profile.hostname;
+    };
+
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToControl = true;
+    };
   };
 
   users.users.${profile.username} = {
