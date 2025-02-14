4818335af5caf9af42e1ef3d2d348950f8146b5d
Andrew Garner <andrew@andrewgarner.com>
2025-02-14 10:21:29 +0000

    Symlink Aerospace config
    This means the aerospace config is symlinked not created in the nix store and therefore has user write permissions.

--- home/aerospace/default.nix
+++ home/aerospace/default.nix
@@ -1,5 +1,5 @@
-{
+{config, ...}: {
   xdg.configFile."aerospace/aerospace.toml" = {
-    source = ./aerospace.toml;
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/code/environment/home/aerospace/aerospace.toml";
   };
 }
