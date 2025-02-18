2afadaac3fb016f7c39cc8c65e5e4dd9f1609bce
Andrew Garner <andrew@andrewgarner.com>
2025-02-18 15:42:54 +0000

    Symlink Ghostty config
    This means the Ghostty config is symlinked not created in the nix store and therefore has user write permissions.

--- home/ghostty/default.nix
+++ home/ghostty/default.nix
@@ -1,5 +1,5 @@
-{
+{config, ...}: {
   xdg.configFile."ghostty/config" = {
-    source = ./config;
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/code/environment/home/ghostty/config";
   };
 }
