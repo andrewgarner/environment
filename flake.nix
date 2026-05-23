01d761e68ee1cf701cc7b2adcb726e9908a685db
Andrew Garner <andrew@andrewgarner.com>
2026-05-23 15:53:59 +0100

    feat(nix): switch to FlakeHub stable, nixfmt, and eval-cores
    - nixpkgs and nix-darwin moved to FlakeHub stable (0)
    - determinate input now follows nixpkgs to reduce duplication
    - formatter switched from alejandra to nixfmt (RFC 166)
    - devShells updated to mkShellNoCC
    - determinateNix.customSettings.eval-cores = 0 for parallel evaluation

--- flake.nix
+++ flake.nix
@@ -2,12 +2,15 @@
   description = "My development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
 
-    determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/3";
+    determinate = {
+      url = "https://flakehub.com/f/DeterminateSystems/determinate/3";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     darwin = {
-      url = "github:nix-darwin/nix-darwin";
+      url = "https://flakehub.com/f/nix-darwin/nix-darwin/0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -17,60 +20,63 @@
     };
   };
 
-  outputs = {
-    self,
-    determinate,
-    darwin,
-    home-manager,
-    nixpkgs,
-    ...
-  }: let
-    system = "aarch64-darwin";
-    pkgs = nixpkgs.legacyPackages.${system};
+  outputs =
+    {
+      self,
+      determinate,
+      darwin,
+      home-manager,
+      nixpkgs,
+      ...
+    }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
 
-    darwinConfiguration = profile:
-      darwin.lib.darwinSystem {
-        inherit system;
+      darwinConfiguration =
+        profile:
+        darwin.lib.darwinSystem {
+          inherit system;
 
-        specialArgs = {
-          inherit profile;
-          configurationRevision = self.rev or self.dirtyRev or null;
-        };
+          specialArgs = {
+            inherit profile;
+            configurationRevision = self.rev or self.dirtyRev or null;
+          };
 
-        modules = [
-          determinate.darwinModules.default
-          ./system
-          ./homebrew
+          modules = [
+            determinate.darwinModules.default
+            ./darwin
 
-          home-manager.darwinModules.home-manager
-          {
-            home-manager = {
-              extraSpecialArgs = {
-                inherit profile;
+            home-manager.darwinModules.home-manager
+            {
+              home-manager = {
+                extraSpecialArgs = {
+                  inherit profile;
+                };
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users.${profile.username} = import ./home;
               };
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users.${profile.username} = import ./home;
-            };
-          }
+            }
+          ];
+        };
+    in
+    {
+      darwinConfigurations = {
+        personal = darwinConfiguration {
+          name = "Andrew Garner";
+          email = "andrew@andrewgarner.com";
+          username = "andrewgarner";
+        };
+      };
+
+      devShells.${system}.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [
+          commitlint-rs
+          lefthook
         ];
       };
-  in {
-    darwinConfigurations = {
-      personal = darwinConfiguration {
-        name = "Andrew Garner";
-        email = "andrew@andrewgarner.com";
-        username = "andrewgarner";
-      };
-    };
 
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
-        commitlint-rs
-        lefthook
-      ];
+      formatter.${system} = pkgs.nixfmt;
     };
-
-    formatter.${system} = pkgs.alejandra;
-  };
 }
